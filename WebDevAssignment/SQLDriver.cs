e02de52f78aaf1562266cdd94609e9bac5f62c7b
Splerge <damnsplurge@gmail.com>
2018-03-07 11:40:09 +1100

    some sql stuff

--- WebDevAssignment/SQLDriver.cs
+++ WebDevAssignment/SQLDriver.cs
@@ -1,11 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Data.SqlClient;
 
 namespace WebDevAssignment
 {
     class SQLDriver
     {
-         private  startConnection
+        private SqlConnection cnn;
+        private string ServerAddress = "wdt2018.australiaeast.cloudapp.azure.com";
+        private string Database = "s3539519";
+        private string Username = "s3539519";
+        private string Password = "abc123";
+
+         private void startConnection()
+        {
+            cnn = new SqlConnection("user id=" + Username + ";" +
+                    "password=" + Password + ";server=" + ServerAddress + ";" +
+                    "Trusted_Connection=yes;" +
+                    "database=" + Username + "; " +
+                    "connection timeout=30");
+            cnn.Open();
+        }
     }
 }
